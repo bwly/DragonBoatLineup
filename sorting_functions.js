df06fc24027eece6d139a369e81a01c511aa3bd7
Teddochi <tedochiai@yahoo.com>
2015-07-15 14:11:44 -0700

    Added builder

---
+++ sorting_functions.js
@@ -0,0 +1,16 @@
+// Provides functions for balancing the weight of the boat
+
+// Makes sure that the weights of paddlers on one side are distributed
+// evenly from the front to the back of the boat
+function sortSingleSide(paddlers){
+	// Starts at middle, then alternates adding forwards and backwards
+	// 5, 6, 4, 7, 3, 8, 2, 9, 1, 10
+	
+	alert(paddlers[0].get('Name'));
+	
+	// Starts at row 5
+	var seat = 5;
+	for(var i = 0; i < paddlers.length; i++){
+		
+	}
+}
